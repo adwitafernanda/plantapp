956be10128001fb0cdab6ace2889e8819335c965
adwitafernanda <adwitafernanda4@gmail.com>
2025-05-22 19:27:31 +0700

    ADD - konten Body dengan header, rekomendasi, dan featured plants

---
+++ lib/components/body.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // It will provie us total height  and width of our screen
+    Size size = MediaQuery.of(context).size;
+    // it enable scrolling on small device
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+           HeaderWithProfileInfo(
+            size: size,
+            name: 'AditGans',
+            address: 'jl.bima 18 ,Tangerang',
+          ),
+          TitleWithMoreBtn(title: "Recomended", press: () {}),
+          RecomendsPlants(),
+          TitleWithMoreBtn(title: "Featured Plants", press: () {}),
+          FeaturedPlants(),
+          SizedBox(height: kDefaultPadding),
+        ],
+      ),
+    );
+  }
+}
